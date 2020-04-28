e453e9e58d4f66b7fc1acbfb05979cc13a5908b7
Stanislas Azanmassou <valdo.azanmassou@yahoo.fr>
2020-04-28 11:31:35 +0100

    Intégration de la gestion des utilisateus

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -5,7 +5,7 @@
           <div class="">
             <div class="page-title">
               <div class="title_left">
-                <h3>Dashboard</h3>
+                <h3>Utilisateurs</h3>
               </div>
 
               <div class="title_right">
@@ -23,7 +23,7 @@
               <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel">
                   <div class="x_title">
-                    <h2>Plain Page</h2>
+                    <h2>Gestion des utilisateurs</h2>
                     <ul class="nav navbar-right panel_toolbox">
                       <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                       </li>
@@ -42,7 +42,9 @@
                     <div class="clearfix"></div>
                   </div>
                   <div class="x_content">
-                      Add content to the page ...
+                      @foreach ($users as $user)
+                          {{ $user->name}}-{{$user->email}}
+                      @endforeach
                   </div>
                 </div>
               </div>
---
+++ resources/views/partials/_sidebar_menu.blade.php
@@ -0,0 +1,15 @@
+            <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+              <div class="menu_section">
+                <h3>General</h3>
+                <ul class="nav side-menu">
+                  <li><a href="{{route('home')}}"><i class="fa fa-home"></i> Dashboard </a></li> 
+                </ul>
+              </div>
+              <div class="menu_section">
+                <h3>Nos activités</h3>
+                <ul class="nav side-menu">
+                  <li><a href="{{route('admin.users.index')}}"><i class="fa fa-users"></i> Gestion des utilisateurs </a></li> 
+                </ul>
+              </div>
+
+            </div>
